623e94a48c564676dcc08de56a42d1787743fa53
ts99ts <108717412+ts99ts@users.noreply.github.com>
2022-07-09 10:37:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,20 +22,20 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+94715842365'] //Owner number in aive msg
+global.owner = ['+94726140613'] //Owner number in aive msg
 global.premium = ['+94726140613'] //Owner Number info
-global.ownernomer = '+94715842365' //Owner Number <<<
+global.ownernomer = '+94726140613' //Owner Number <<<
 
 global.ownername = 'ts99ts' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.botname = 'Creamy BT' //Bot Name
 
 global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
 global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = 'Nz.' //Bot Pacage Name
+global.author = 'ST.' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
